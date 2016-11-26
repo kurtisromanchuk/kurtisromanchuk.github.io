e4813f2fafdc241a660ff7ef329e466b50f923b2
Kurtis Romanchuk <kurtis.romanchuk@gmail.com>
2016-11-25 19:59:54 -0500

    Worked on installing gulp and dependancies

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+// Kurtis Romanchuk gulpfile.js created mostly with help from https://css-tricks.com/gulp-for-beginners/
+
+// Requires gulp
+var gulp = require('gulp');
+// Requires the gulp sass plugin
+var sass = require('gulp-sass');
+// Requires the browser-sync plugin
+var browserSync = require('browser-sync').create();
+// Requires useref
+var useref = require('gulp-useref');
+// Requirements for minifying javascript
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+// Creates function to convert sass to css
+gulp.task('sass', function(){
+	return gulp.src('scss/**/*.scss')
+		.pipe(sass()) // Converts sass to css with gulp-sass
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+});
+
+// Browser Sync
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: {
+			baseDir: '.'
+		},
+	})
+})
+
+// Watch task
+gulp.task('watch', ['browserSync', 'sass'], function(){
+	// Automatically update scss files to css
+	gulp.watch('scss/**/*.scss', ['sass']);
+	// Reload browser whenever html, css, or js files change
+	gulp.watch('*.html', browserSync.reload);
+	gulp.watch('css/*.css', browserSync.reload);
+	gulp.watch('js/*.js', browserSync.reload);
+})
+
+// Creates useref task to build javascript libraries at end of html file into correct order
+gulp.task('useref', function(){
+	return gulp.src('*.html')
+		.pipe(useref())
+		// Minifies only if it's a Javascript file
+		.pipe(gulpIf('*.js', uglify()))
+		.pipe(gulp.dest('dist'))
+});
+
