a5ac17231eea3598d8d5b9c0c6fdbac2290dc02e
Kurtis Romanchuk <kurtis.romanchuk@gmail.com>
2016-12-05 02:27:02 -0500

    Added mobile menu

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,5 @@
 (function($) {
+  
   $(function() {
     $('nav ul li > a:not(:only-child)').click(function(e) {
       $(this).siblings('.jadegreen-layout-navdropdown').toggle();
@@ -9,4 +10,14 @@
       $('.jadegreen-layout-navdropdown').hide();
     });
   });
+
+
+  $('#jadegreen-layout-navtoggle').on('click', function() {
+    this.classList.toggle('active');
+  });
+
+  $('#jadegreen-layout-navtoggle').click(function() {
+    $('nav ul').toggle();
+  });
+
 })(jQuery);
